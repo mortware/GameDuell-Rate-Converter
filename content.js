aa6be41eb7500c04b0cf60a51b582fe0a441a5e7
David Price <david@mortware.net>
2017-01-12 19:30:17 +0000

    Added 'mouseout' feature
    Reverts the original value when user moves mouse out of the converted
    element
    Added a currencies object for possible future rate conversions

--- content.js
+++ content.js
@@ -1,17 +1,57 @@
+var attributeName = 'data-original-value',
+	currencies = {
+		gbp: { 
+			rate: 0.8672500000000001,
+			symbol: '£ ',
+			symbolFirst: true
+		}
+	}
+
 document.addEventListener('mouseover', function(e){
 	var srcElement = e.srcElement;
-
-	var name = srcElement.nodeName;
-
 	if (srcElement.innerHTML.startsWith("G£")) {
+		var originalValue = getValueFromString(srcElement.innerText);
+		srcElement.setAttribute(attributeName, srcElement.innerText);
 
-		var value = Number(srcElement.innerHTML.replace(/\s|£|G|,/g, ''));
-		value = value / 100;
-		value = value * 0.8672500000000001;
-
-		value = value.toFixed(2);
-		var valueText = '£ ' + value;
-		srcElement.innerHTML = valueText;
+		var newValue = convertToCurrencyString(originalValue, 'gbp');
+		srcElement.innerText = newValue;
 	};
 	
 }, false);
+
+document.addEventListener('mouseout', function(e){
+	var srcElement = e.srcElement;
+	if (srcElement.hasAttribute(attributeName)) {
+		var originalValue = srcElement.getAttribute(attributeName);
+		srcElement.innerText = originalValue;
+	}
+
+}, false);
+
+function getValueFromString(value){
+	var newValue = Number(value.replace(/\s|£|G|,/g, ''));
+	return newValue;
+}
+
+function convertToCurrencyString(value, currencyCode){
+
+	var newValue;
+	if (currencyCode) {
+		var currency = currencies[currencyCode];
+		newValue = value / 100;
+		newValue = newValue * currency.rate;
+		newValue = newValue.toFixed(2);
+
+		if (currency.symbolFirst) {
+			newValue = currency.symbol + newValue;
+		} else {
+			newValue = newValue + currency.symbol;
+		}
+		
+	}
+	else {
+		newValue = value;
+	}
+
+	return newValue;
+}
